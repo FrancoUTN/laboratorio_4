d9c4028f42c07d65c94d92ddeccfd0519f4923bb
unknown <cataniafrancoar@gmail.com>
2022-04-07 02:46:35 -0300

    Corrección Heroku y suma

---
+++ src/app/Entidades/suma.ts
@@ -0,0 +1,10 @@
+export class Suma {
+    
+    num1:number = 0;
+    num2:number = 0;
+    num3:number = 0;    
+    
+    public sumar() {
+      this.num3 = this.num1 + this.num2;
+    }  
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Suma } from './Entidades/suma';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicios-clases';
+
+  miSuma:Suma;
+
+  constructor() {
+    this.miSuma = new Suma();
+  }
+
 }
